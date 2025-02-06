2d8513a6efdf16199bb0cc3caa6e0f67d46ea672
meet-gg <meetgoyanip@gmail.com>
2025-02-06 22:34:05 +0530

    add RestaurantServices

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/RestaurantServices.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/RestaurantServices.java
@@ -5,10 +5,10 @@ import com.Online.Food.Delivery.System.Online.Food.DTO.RestaurantDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface RestaurantServices {
-    public ResponseEntity<?> addRestaurant(RestaurantDTO restaurantDTO);
-    public ResponseEntity<?> assignFoodToRestaurant(Long id, MenuDTO menuDTO);
-    public ResponseEntity<?> getOrdersByRestaurantId(Long restaurantId);
-    public ResponseEntity<?> orderReady(Long orderId);
-    public ResponseEntity<?> OrderDelivered(Long orderId,Integer otp);
+     ResponseEntity<?> addRestaurant(RestaurantDTO restaurantDTO);
+     ResponseEntity<?> assignFoodToRestaurant(Long id, MenuDTO menuDTO);
+     ResponseEntity<?> getOrdersByRestaurantId(Long restaurantId);
+     ResponseEntity<?> orderReady(Long orderId);
+     ResponseEntity<?> OrderDelivered(Long orderId,Integer otp);
 
 }
