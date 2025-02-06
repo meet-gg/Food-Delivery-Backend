217b8f31405ce17662660a6f411e88d8fb9a31d7
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:40:11 +0530

    add OnlineFoodDeliveryAppTesting

---
+++ src/test/java/com/Online/Food/Delivery/System/Online/Food/OnlineFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online.Food.Delivery.System.Online.Food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
