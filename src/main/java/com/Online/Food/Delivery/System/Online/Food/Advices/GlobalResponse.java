3ac7fb9358c81f9f52e05b2f83b97cd531e6cda7
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:19:38 +0530

    add Api responses

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Advices/GlobalResponse.java
@@ -0,0 +1,38 @@
+package com.Online.Food.Delivery.System.Online.Food.Advices;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class GlobalResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true; // Apply to all responses
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response) {
+
+        if (body instanceof byte[]) {
+            return body;  // Return the raw image data as is
+        }
+
+        if (body instanceof ApiResponse<?> || body instanceof String) {
+            return body;
+        }
+
+        return new ApiResponse<>(body);
+    }
+}
