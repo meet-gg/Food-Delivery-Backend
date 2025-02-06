6a0fc09d10a7b52f5c79267a5742ab1b3685325d
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:23:12 +0530

    add DeliveryPersonDTO Entity

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/DTO/DeliveryPersonDTO.java
@@ -0,0 +1,15 @@
+package com.Online.Food.Delivery.System.Online.Food.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryPersonDTO {
+    private Long id;
+    private String phoneNumber;
+}
