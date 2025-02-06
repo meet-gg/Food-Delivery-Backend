f362825493b9881edd5be5248e359064ced3b24f
meet-gg <meetgoyanip@gmail.com>
2025-02-06 22:36:05 +0530

    add AuthServices

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/AuthServices.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/AuthServices.java
@@ -4,6 +4,6 @@ import com.Online.Food.Delivery.System.Online.Food.DTO.LoginDTO;
 import com.Online.Food.Delivery.System.Online.Food.DTO.LoginResponseDTO;
 
 public interface AuthServices {
-    public LoginResponseDTO login(LoginDTO loginDTO);
-    public LoginResponseDTO refreshToken(String refreshToken);
+     LoginResponseDTO login(LoginDTO loginDTO);
+     LoginResponseDTO refreshToken(String refreshToken);
 }
