58a3eb575a800287dd76616ccdf77a23508bc4c6
meet-gg <meetgoyanip@gmail.com>
2025-02-06 22:36:14 +0530

    add UserServices

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/UserServices.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Services/UserServices.java
@@ -4,5 +4,5 @@ import com.Online.Food.Delivery.System.Online.Food.DTO.SignUpDTO;
 import com.Online.Food.Delivery.System.Online.Food.DTO.UserDTO;
 
 public interface UserServices {
-    public UserDTO signup(SignUpDTO signUpDTO);
+     UserDTO signup(SignUpDTO signUpDTO);
 }
