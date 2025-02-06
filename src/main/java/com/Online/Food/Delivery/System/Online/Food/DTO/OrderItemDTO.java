57295f055ed8eede6bf7a98fdb31393ffc576eac
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:34:44 +0530

    add orderItemsDTO for use of response relation with Order

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/DTO/OrderItemDTO.java
@@ -0,0 +1,17 @@
+package com.Online.Food.Delivery.System.Online.Food.DTO;
+
+import com.Online.Food.Delivery.System.Online.Food.Entity.Menu;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+    private Menu menu;
+    private Integer quantity;
+}
