f43b82a2c9a41825bb362b75fa81183a3acac985
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:35:57 +0530

    add PaymentDTO Entity

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/DTO/PaymentDTO.java
@@ -0,0 +1,8 @@
+//package com.Online.Food.Delivery.System.Online.Food.DTO;
+//
+//public class PaymentDTO {
+//    private Long id;
+//    private Double amount;
+//    private String paymentStatus;
+//    private Long orderId;
+//}
