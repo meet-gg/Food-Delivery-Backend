6bdf7f8627a41a39cc98be6c87f068a4b2d01983
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:41:21 +0530

    add Food delivery app

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Online.Food.Delivery.System.Online.Food.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Utils/RestaurantSecurity.java
@@ -0,0 +1,11 @@
+//package com.Online.Food.Delivery.System.Online.Food.Utils;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RestaurantSecurity {
+//
+//    public boolean isUseris
+//}
