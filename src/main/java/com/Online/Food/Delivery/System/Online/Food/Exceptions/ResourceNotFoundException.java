c1bbc4f140ce6324ba145333c4ccf0ad4b97f1a7
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:46:04 +0530

    add Custom Exception class

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Exceptions/ResourceNotFoundException.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Exceptions/ResourceNotFoundException.java
@@ -1,7 +1,6 @@
 package com.Online.Food.Delivery.System.Online.Food.Exceptions;
 
 public class ResourceNotFoundException extends RuntimeException{
-
     public ResourceNotFoundException(String message) {
         super(message);
     }
