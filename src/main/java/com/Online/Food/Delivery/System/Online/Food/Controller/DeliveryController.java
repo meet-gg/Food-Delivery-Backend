463b880a099df1c3f5895b49ee5ee25d24631ee6
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:21:49 +0530

    add Delivery Controller (Api endpoints)

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/DeliveryController.java
@@ -0,0 +1,44 @@
+package com.Online.Food.Delivery.System.Online.Food.Controller;
+
+import com.Online.Food.Delivery.System.Online.Food.DTO.DeliveryPersonDTO;
+import com.Online.Food.Delivery.System.Online.Food.Entity.Delivery;
+import com.Online.Food.Delivery.System.Online.Food.Services.DeliveryPersonServices;
+import com.Online.Food.Delivery.System.Online.Food.Services.DeliveryServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/delivery-person")
+@RequiredArgsConstructor
+public class DeliveryController {
+    private final DeliveryServices deliveryServices;
+    private final DeliveryPersonServices deliveryPersonServices;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody DeliveryPersonDTO deliveryPerson) {
+        ResponseEntity<?> save = deliveryPersonServices.save(deliveryPerson);
+        return ResponseEntity.ok(save);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAll() {
+        ResponseEntity<?> allOrder = deliveryServices.getAllOrder();
+        return ResponseEntity.ok(allOrder.getBody());
+    }
+
+//    @DeleteMapping("/deleteByOrderId/{id}")
+//    public ResponseEntity<?> deleteByOrderId(@PathVariable Long id) {
+//        ResponseEntity<?> order = deliveryServices.removeDeliverOrder(id);
+//        return ResponseEntity.ok(order);
+//    }
+
+    @GetMapping("/assignD/{deliveryId}")
+    public ResponseEntity<?> assignDelivery(@PathVariable Long deliveryId) {
+        ResponseEntity<?> order = deliveryServices.assignDelivery(deliveryId);
+        return ResponseEntity.ok(order);
+    }
+
+}
