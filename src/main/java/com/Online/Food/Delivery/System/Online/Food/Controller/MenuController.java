eda58c5d92fff21f016e64e0ab99f5c282c2c5fc
meet-gg <meetgoyanip@gmail.com>
2025-02-06 21:37:50 +0530

    add Menu Controller

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/MenuController.java
@@ -0,0 +1,28 @@
+//package com.Online.Food.Delivery.System.Online.Food.Controller;
+//
+//import com.Online.Food.Delivery.System.Online.Food.DTO.MenuDTO;
+//import com.Online.Food.Delivery.System.Online.Food.Services.MenuServices;
+//import lombok.RequiredArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("menu")
+//public class MenuController {
+//
+//    private static final Logger log = LoggerFactory.getLogger(MenuController.class);
+//    private final MenuServices menuServices;
+//
+//    @PostMapping("/add")
+//    public ResponseEntity<?> createMenu(@RequestBody MenuDTO menuDTO) {
+//        log.info("controller {}",menuDTO);
+//        ResponseEntity<?> menu = menuServices.createMenu(menuDTO);
+//        return ResponseEntity.ok(menu);
+//    }
+//}
