77f7ec0d65a34ef55696b2e84344b87ea76632fd
meet-gg <meetgoyanip@gmail.com>
2025-02-07 17:00:17 +0530

    response for restaurantMenu

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/DTO/RestroMenuResponseDTO.java
@@ -0,0 +1,8 @@
+package com.Online.Food.Delivery.System.Online.Food.DTO;
+
+import com.Online.Food.Delivery.System.Online.Food.Entity.Menu;
+
+public class RestroMenuResponseDTO {
+    private Menu menu;
+    private Integer quantity;
+}
