e939eb71fbd74f9db84477f37e510d82bac8a00f
meet-gg <meetgoyanip@gmail.com>
2025-02-08 16:28:15 +0530

    add preauthorize on checkStatus endpoint

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/CustomerController.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.Online.Food.Delivery.System.Online.Food.Services.RestaurantServices;
 import com.Online.Food.Delivery.System.Online.Food.Services.RestaurantServicesImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +19,14 @@ public class CustomerController {
     private final RestaurantServices restaurantServices;
     private final DeliveryServices deliveryServices;
     private final MenuServices menuServices;
-    @GetMapping("/orderDelivered/{orderId}/{otp}")
-    public ResponseEntity<?> orderAccept(@PathVariable Long orderId, @PathVariable Integer otp) {
-        ResponseEntity<?> orderReady = restaurantServices.OrderDelivered(orderId,otp);
-        return ResponseEntity.ok(orderReady);
-    }
-    @GetMapping("/checkStatus/{deliveryId}")
-    public ResponseEntity<?> checkStatus(@PathVariable Long deliveryId) {
-        ResponseEntity<?> deliveryById = deliveryServices.getDeliveryById(deliveryId);
+
+    @GetMapping("/checkStatus/{orderId}")
+    @PreAuthorize("@customerSecurity.isOrderExist(#orderId)")
+    public ResponseEntity<?> checkStatus(@PathVariable Long orderId) {
+        ResponseEntity<?> deliveryById = deliveryServices.getDeliveryById(orderId);
         return ResponseEntity.ok(deliveryById);
     }
+
     @GetMapping("/getMenuByRestaurantId/{restaurantId}")
     public ResponseEntity<?> getMenuByRestaurantId(@PathVariable Long restaurantId) {
         ResponseEntity<?> menuByRestaurantId = menuServices.getMenuByRestaurantId(restaurantId);
