b0115eb1c363b7d5458935246caf6b56085d7a21
meet-gg <meetgoyanip@gmail.com>
2025-02-08 16:29:10 +0530

    make method Optional

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Repository/DeliveryPersonRepository.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Repository/DeliveryPersonRepository.java
@@ -3,6 +3,8 @@ package com.Online.Food.Delivery.System.Online.Food.Repository;
 import com.Online.Food.Delivery.System.Online.Food.Entity.DeliveryPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeliveryPersonRepository extends JpaRepository<DeliveryPerson, Long> {
-    DeliveryPerson findByEmail(String email);
+    Optional<DeliveryPerson> findByEmail(String email);
 }
