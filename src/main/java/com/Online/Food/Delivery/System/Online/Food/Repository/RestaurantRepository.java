79586b8c6bf5adc5faa18f6ba4e1b41a7e4f36b7
meet-gg <meetgoyanip@gmail.com>
2025-02-08 16:30:54 +0530

    add

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Repository/RestaurantRepository.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Repository/RestaurantRepository.java
@@ -1,11 +1,8 @@
 package com.Online.Food.Delivery.System.Online.Food.Repository;
 
-import com.Online.Food.Delivery.System.Online.Food.Entity.Order;
 import com.Online.Food.Delivery.System.Online.Food.Entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
