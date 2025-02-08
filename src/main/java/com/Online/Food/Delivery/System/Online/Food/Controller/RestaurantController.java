1efe1335dc2111aef1e4327eebf1efbd328d201b
meet-gg <meetgoyanip@gmail.com>
2025-02-08 16:29:56 +0530

    make preauthorize and Swagger-ui endpoint description

--- src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/RestaurantController.java
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Controller/RestaurantController.java
@@ -2,10 +2,13 @@ package com.Online.Food.Delivery.System.Online.Food.Controller;
 
 import com.Online.Food.Delivery.System.Online.Food.DTO.MenuDTO;
 import com.Online.Food.Delivery.System.Online.Food.DTO.RestaurantDTO;
+import com.Online.Food.Delivery.System.Online.Food.Services.DeliveryServices;
 import com.Online.Food.Delivery.System.Online.Food.Services.RestaurantServices;
 import com.Online.Food.Delivery.System.Online.Food.Services.RestaurantServicesImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,14 +16,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantServices restaurantServices;
+    private final DeliveryServices deliveryServices;
 
     @PostMapping("/addRest")
+    @Operation(summary = "add Restaurant")  // for use of Swagger ui
     public ResponseEntity<?> addRest(@RequestBody RestaurantDTO restaurantDTO) {
         ResponseEntity<?> restaurantDTO1= restaurantServices.addRestaurant(restaurantDTO);
         return ResponseEntity.ok(restaurantDTO1);
     }
 
     @PostMapping("/assignFood/{restaurantId}")
+    @PreAuthorize("@restaurantSecurity.isValidRestaurantAddFood(#restaurantId)")
     public ResponseEntity<?> assignFood(@PathVariable Long restaurantId, @RequestBody MenuDTO menuDTO) {
         ResponseEntity<?> restaurantDTO1= restaurantServices.assignFoodToRestaurant(restaurantId,menuDTO);
         return ResponseEntity.ok(restaurantDTO1);
@@ -32,12 +38,18 @@ public class RestaurantController {
         return ResponseEntity.ok(orders);
     }
 
+
     @GetMapping("/orderOutOfDelivery/{orderId}")
+    @PreAuthorize("@restaurantSecurity.isUserOrderExist(#orderId)")
     public ResponseEntity<?> orderOutOfDelivery(@PathVariable Long orderId) {
         ResponseEntity<?> orderReady = restaurantServices.orderReady(orderId);
         return ResponseEntity.ok(orderReady);
     }
 
-
-
+    @GetMapping("/checkStatus/{orderId}")
+    @PreAuthorize("@restaurantSecurity.isUserOrderExist(#orderId)")
+    public ResponseEntity<?> checkStatus(@PathVariable Long orderId) {
+        ResponseEntity<?> deliveryById = deliveryServices.getDeliveryById(orderId);
+        return ResponseEntity.ok(deliveryById);
+    }
 }
