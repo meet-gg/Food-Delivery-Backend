c1b68edeac10a913ff1c934b4d81a6e50690de6c
meet-gg <meetgoyanip@gmail.com>
2025-02-10 18:04:44 +0530

    add Session

---
+++ src/main/java/com/Online/Food/Delivery/System/Online/Food/Entity/Session.java
@@ -0,0 +1,24 @@
+package com.Online.Food.Delivery.System.Online.Food.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String refreshToken;
+    @CreationTimestamp
+    private LocalDateTime lastUsedAt;
+    @ManyToOne
+    private User user;
+}
